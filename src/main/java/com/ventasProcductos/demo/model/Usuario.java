6c83dcf62545acb03c0d8f0e0cc0a026f38fa873
Daniel <Daniel3204155185@gmail.com>
2025-03-03 18:51:19 -0500

    daniel lo logro

--- src/main/java/com/ventasProcductos/demo/model/Usuario.java
+++ src/main/java/com/ventasProcductos/demo/model/Usuario.java
@@ -1,30 +1,22 @@
 package com.ventasProcductos.demo.model;
 
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
-import java.util.List;
 
 @Entity
-@Table(name = "usuario")
 public class Usuario {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id_usuario")
     private int id;
 
-    @Column(name = "nombre", nullable = false, length = 100)
     private String nombre;
 
-    @OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-    private List<Venta> ventas;
+    // Constructor sin argumentos
+    public Usuario() {
+    }
 
     // Getters y Setters
     public int getId() {
@@ -41,13 +33,5 @@ public class Usuario {
 
     public void setNombre(String nombre) {
         this.nombre = nombre;
-    }
-
-    public List<Venta> getVentas() {
-        return ventas;
-    }
-
-    public void setVentas(List<Venta> ventas) {
-        this.ventas = ventas;
     }
 }
