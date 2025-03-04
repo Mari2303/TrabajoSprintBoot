d03ed869d1104e02c7586e2fd4179a3227855e3b
Daniel <Daniel3204155185@gmail.com>
2025-03-04 08:52:34 -0500

    producto

---
+++ src/main/java/com/ventasProcductos/demo/controller/ProductoPageController.java
@@ -0,0 +1,15 @@
+package com.ventasProcductos.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/productos")
+public class ProductoPageController {
+
+    @GetMapping
+    public String productosPage() {
+        return "productos.html";
+    }
+}
