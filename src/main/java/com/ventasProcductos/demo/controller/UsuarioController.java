6ad26c7376457eb454f911d3b12e49e3fe3e1d3d
Daniel <Daniel3204155185@gmail.com>
2025-03-04 08:11:12 -0500

    nuevos cambios

--- src/main/java/com/ventasProcductos/demo/controller/UsuarioController.java
+++ src/main/java/com/ventasProcductos/demo/controller/UsuarioController.java
@@ -58,3 +58,4 @@ public class UsuarioController {
         return ResponseEntity.noContent().build();
     }
 }
+
---
+++ src/main/java/com/ventasProcductos/demo/controller/UsuarioPageController.java
@@ -0,0 +1,15 @@
+package com.ventasProcductos.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/usuarios")
+public class UsuarioPageController {
+
+    @GetMapping
+    public String usuariosPage() {
+        return "index";
+    }
+}
