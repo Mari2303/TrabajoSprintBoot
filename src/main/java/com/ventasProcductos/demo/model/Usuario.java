120f22e7249566804bd9188e6e560759f288face
Daniel <Daniel3204155185@gmail.com>
2025-03-09 14:26:10 -0500

    ventas funciona correctamente

--- src/main/java/com/ventasProcductos/demo/model/Usuario.java
+++ src/main/java/com/ventasProcductos/demo/model/Usuario.java
@@ -1,11 +1,14 @@
 package com.ventasProcductos.demo.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class Usuario {
 
     @Id
--- src/main/java/com/ventasProcductos/demo/model/Venta.java
+++ src/main/java/com/ventasProcductos/demo/model/Venta.java
@@ -1,15 +1,19 @@
 package com.ventasProcductos.demo.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Id;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import java.util.Date;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
 @Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class Venta {
 
     @Id
